0e18e78a3586fada12d7d554216c311010f465c0
whuang8 <huang554@purdue.edu>
2017-02-05 16:45:03 -0500

    Add title attribute to course

--- Sources/App/Models/Course.swift
+++ Sources/App/Models/Course.swift
@@ -6,17 +6,20 @@ final class Course: Model {
     var exists: Bool = false
     
     var name: String
+    var title: String
     var enrollment: Int
     
-    init(name: String, enrollment: Int) {
+    init(name: String, title: String, enrollment: Int) {
         self.id = nil
         self.name = name
+        self.title = title
         self.enrollment = enrollment
     }
     
     init(node: Node, in context: Context) throws {
         self.id = try node.extract("id")
         self.name = try node.extract("name")
+        self.title = try node.extract("title")
         self.enrollment = try node.extract("enrollment")
     }
     
@@ -24,6 +27,7 @@ final class Course: Model {
         return try Node(node: [
             "id": id,
             "name": name,
+            "title": title,
             "enrollment": enrollment
         ])
     }
@@ -32,6 +36,7 @@ final class Course: Model {
         try database.create("courses", closure: { (courses) in
             courses.id()
             courses.string("name", length: nil, optional: false, unique: true, default: nil)
+            courses.string("title", length: nil, optional: true, unique: false, default: nil)
             courses.int("enrollment", optional: true)
         })
     }
