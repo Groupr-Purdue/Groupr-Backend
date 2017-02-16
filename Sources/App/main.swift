bc489535335f47d874e37f24c0562004cff649cf
whuang8 <huang554@purdue.edu>
2017-02-16 16:50:43 -0500

    Add postgresql

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,12 +1,12 @@
 import Vapor
-import VaporSQLite
+import VaporPostgreSQL
 import Fluent
 import Library
 
 // Initialize root Droplet.
 let drop = Droplet()
 drop.middleware.insert(CORSMiddleware(), at: 0)
-try drop.addProvider(VaporSQLite.Provider.self)
+try drop.addProvider(VaporPostgreSQL.Provider.self)
 
 // Prepare the SQLite DB if needed on boot.
 // TODO: Move to Model classes.
