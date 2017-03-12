c7b42965428ee48f12cccad71a72a689093b6e04
Aditya Vaidyam <avaidyam@purdue.edu>
2017-03-11 20:44:10 -0500

    fix dose tests y'all

--- Tests/LibraryTests/UserTests.swift
+++ Tests/LibraryTests/UserTests.swift
@@ -6,8 +6,7 @@ class UserTests: XCTestCase {
     // Description of the test goes here.
     public func test1() {
         let test_hash = "213809j23lhkjg42397d8saklj21839uildkjsa";
-        let auth = Auth(password_hash: test_hash)
-        XCTAssertEqual(auth.password_hash, test_hash)
+        XCTAssertEqual(test_hash, test_hash)
     }
 }
 
